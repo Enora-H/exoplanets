a860101c372a16fb0dd9c3ee76b36a6e845292fe
Enora-H <123633277+Enora-H@users.noreply.github.com>
2024-11-26 16:38:59 +0100

    Ajout premier fichier de code

---
+++ Exoplanet.py
@@ -0,0 +1,217 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf 
+from tensorflow.keras.models import load_model
+import random 
+import os
+from tensorflow.keras.losses import MeanAbsoluteError
+from matplotlib.ticker import ScalarFormatter
+import pandas as pd
+do_the_mcdropout_wc = True
+do_the_mcdropout = True
+#labels=pd.read_csv("C:\\Users\\marca\\Desktop\\École\\2IA\\exoplanet\\train_labels.csv\\train_labels.csv")
+#print(labels)
+train_solution = np.loadtxt("C:\\Users\\marca\\Desktop\\École\\2IA\\exoplanet\\train_labels.csv\\train_labels.csv", delimiter = ',', skiprows = 1)
+targets = train_solution[:,1:]
+targets_mean = targets[:,1:].mean(axis = 1)
+N = targets.shape[0]
+
+df=np.load("C:\\Users\\marca\\Desktop\\École\\2IA\\exoplanet\\data_train.npy\\data_train.npy")
+dfFGS=np.load("C:\\Users\\marca\\Desktop\\École\\2IA\\exoplanet\\data_train_FGS.npy")
+
+signal_AIRS_diff_transposed_binned, signal_FGS_diff_transposed_binned=df,dfFGS
+FGS_column = signal_FGS_diff_transposed_binned.sum(axis = 2)
+#on rajoute aux AIRS une colonne de FGS (voir avec Claire pour l'explication sur les intervalles pris en compte)
+dataset = np.concatenate([signal_AIRS_diff_transposed_binned, FGS_column[:,:, np.newaxis,:]], axis = 2)
+dataset = dataset.sum(axis=3)
+
+max_size = 10000
+dataset = dataset[:max_size]
+targets = targets[:max_size]
+
+#on normalise entre 0 et 1
+def create_dataset_norm(dataset1, dataset2) :
+    dataset_norm1 = np.zeros(dataset1.shape)
+    dataset_norm2 = np.zeros(dataset1.shape)
+    dataset_min = dataset1.min()
+    dataset_max = dataset1.max()
+    dataset_norm1 = (dataset1 - dataset_min) / (dataset_max - dataset_min)
+    dataset_norm2 = (dataset2 - dataset_min) / (dataset_max - dataset_min)
+    return dataset_norm1, dataset_norm2
+
+#on normalise le spectre en le divisant par la moyenne des 50 premiers et 50 dernier points
+def norm_star_spectrum (signal) : 
+    img_star = signal[:,:50].mean(axis = 1) + signal[:,-50:].mean(axis = 1)
+    return signal/img_star[:,np.newaxis,:]
+
+dataset_norm = norm_star_spectrum(dataset)
+dataset_norm = np.transpose(dataset_norm,(0,2,1))
+
+#on garde que certaines longueurs d'ondes
+cut_inf, cut_sup = 39, 321 # we have previously cut the data along the wavelengths to remove the edges, this is to match with the targets range in the make data file
+l = cut_sup - cut_inf + 1 
+wls = np.arange(l)
+
+#on sépare les données en train et test
+def split (data, N) : 
+    list_planets = random.sample(range(0, data.shape[0]), N_train)
+    list_index_1 = np.zeros(data.shape[0], dtype = bool)
+    for planet in list_planets : 
+        list_index_1[planet] = True
+    data_1 = data[list_index_1]
+    data_2 = data[~list_index_1]
+    return data_1, data_2, list_index_1
+
+N_train = 8*N//10
+
+# Validation and train data split
+train_obs, valid_obs, list_index_train = split(dataset_norm, N_train)
+train_targets, valid_targets = targets[list_index_train], targets[~list_index_train]
+
+#préparation de la courbe blanche
+signal_AIRS_diff_transposed_binned = signal_AIRS_diff_transposed_binned.sum(axis=3)
+wc_mean = signal_AIRS_diff_transposed_binned.mean(axis=1).mean(axis=1)
+white_curve = signal_AIRS_diff_transposed_binned.sum(axis=2)/ wc_mean[:, np.newaxis]
+
+#on normalise encore
+def normalise_wlc(train, valid) :
+    wlc_train_min = train.min()
+    wlc_train_max = train.max()
+    train_norm = (train - wlc_train_min) / (wlc_train_max - wlc_train_min)
+    valid_norm = (valid - wlc_train_min) / (wlc_train_max - wlc_train_min)
+    
+    return train_norm, valid_norm
+
+#et on re-splite et normalise
+def normalize (train, valid) : 
+    max_train = train.max()
+    min_train = train.min()
+    train_norm = (train - min_train) / (max_train - min_train)
+    valid_norm = (valid - min_train) / (max_train - min_train)
+    return train_norm, valid_norm, min_train, max_train
+train_wc, valid_wc = white_curve[list_index_train], white_curve[~list_index_train]
+train_targets_wc, valid_targets_wc = targets_mean[list_index_train], targets_mean[~list_index_train]
+train_wc, valid_wc = normalise_wlc(train_wc, valid_wc)
+train_targets_wc_norm, valid_targets_wc_norm, min_train_valid_wc, max_train_valid_wc = normalize(train_targets_wc, valid_targets_wc)
+
+#plt.figure()
+#for i in range (200) : 
+#    plt.plot(train_wc[-i], '-', alpha = 0.5)
+#plt.title('Light-curves from the train set') 
+#plt.xlabel('Time')
+#plt.ylabel('Normalized flux')
+#plt.show()
+
+from keras.layers import Input, Conv1D, MaxPooling1D, Flatten, Dense, Dropout, BatchNormalization, Concatenate,AveragePooling1D
+from keras.models import Model, load_model
+from tensorflow.keras.optimizers import Adam, SGD
+from tensorflow.keras.callbacks import LearningRateScheduler, ModelCheckpoint
+
+#1D CNN
+input_wc = Input((187,1))
+x = Conv1D(32, 3, activation='relu')(input_wc)
+x = MaxPooling1D()(x)
+x = BatchNormalization() (x)
+x = Conv1D(64, 3, activation='relu')(x)
+x = MaxPooling1D()(x)
+x = Conv1D(128, 3, activation='relu')(x)
+x = MaxPooling1D()(x)
+x = Conv1D(256, 3, activation='relu')(x)
+x = MaxPooling1D()(x)
+x = Flatten()(x)
+
+x = Dense(500, activation='relu')(x)
+x = Dropout(0.2)(x, training = True)
+x = Dense(100, activation='relu')(x)
+x = Dropout(0.1)(x, training = True)
+output_wc = Dense(1, activation='linear')(x)
+
+model_wc = Model(inputs=input_wc, outputs=output_wc)
+model_wc.summary()
+
+def scheduler(epoch, lr):
+    decay_rate = 0.2
+    decay_step = 200  
+    if epoch % decay_step == 0 and epoch:
+        return lr * decay_rate
+    return lr
+
+#ça train un max
+optimizer = SGD(0.001)
+model_wc.compile(optimizer=optimizer, loss='mse', metrics=[MeanAbsoluteError()])
+callback = LearningRateScheduler(scheduler)
+checkpoint_filepath = 'output/model_1dcnn.keras'
+model_ckt = ModelCheckpoint(
+    checkpoint_filepath,
+    monitor="val_loss",
+    verbose=0,
+    save_best_only=True,
+    save_weights_only=False,
+    mode="min",
+    save_freq="epoch",
+)
+
+print('Running ...')
+history = model_wc.fit(
+    x = train_wc,  
+    y = train_targets_wc_norm,
+    validation_data = (valid_wc, valid_targets_wc_norm),  
+    batch_size=16,
+    epochs= 1200,
+    shuffle=True,
+    verbose=0, 
+    callbacks=[model_ckt]
+    )
+print('Done.')
+
+
+nb_dropout_wc = 1
+
+def unstandardizing (data, min_train_valid, max_train_valid) : 
+    return data * (max_train_valid - min_train_valid) + min_train_valid
+
+#uncertainty avec Monte-Carlo
+def MC_dropout_WC (model, data, nb_dropout) : 
+    predictions = np.zeros((nb_dropout, data.shape[0]))
+    for i in range(nb_dropout) : 
+        predictions[i,:] = model.predict(data, verbose = 0).flatten()
+    return predictions
+
+if do_the_mcdropout_wc :
+    print('Running ...')
+    prediction_valid_wc = MC_dropout_WC(model_wc, valid_wc, nb_dropout_wc)
+    spectre_valid_wc_all = unstandardizing(prediction_valid_wc, min_train_valid_wc, max_train_valid_wc)
+    spectre_valid_wc, spectre_valid_std_wc = spectre_valid_wc_all.mean(axis = 0), spectre_valid_wc_all.std(axis = 0)
+    print('Done.')
+
+else : 
+    spectre_valid_wc = model_wc.predict(valid_wc).flatten()
+    spectre_valid_wc = unstandardizing(spectre_valid_wc, min_train_valid_wc, max_train_valid_wc)
+    spectre_valid_std_wc = 0.1*np.abs(spectre_valid_wc)
+
+residuals = spectre_valid_wc - valid_targets_wc
+
+#on plot les résultats finaux
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 6), sharex=True,
+                               gridspec_kw={'height_ratios': [3, 1]})
+
+ax1.errorbar(x = np.arange(len(spectre_valid_wc)), y = spectre_valid_wc, yerr =spectre_valid_std_wc, fmt = '.', color = 'k', ecolor = 'gray', label='Prediction', alpha=0.8)
+ax1.fill_between(np.arange(len(spectre_valid_wc)), spectre_valid_wc - spectre_valid_std_wc, spectre_valid_wc + spectre_valid_std_wc, color = 'grey', alpha = 0.5)
+ax1.vlines(np.arange(len(spectre_valid_wc)),ymin=0, ymax=spectre_valid_wc, colors='r', linestyle='dashed',alpha = 0.1)
+ax1.plot(valid_targets_wc, 'r.', label='Target', alpha=0.8)
+ax1.set_xlabel('Concatenated targets')
+ax1.set_ylabel('$(R_p/R_s)^2$')
+ax1.set_title('Prediction vs target, mean value of the spectrum, on validation dataset')
+ax1.legend()
+
+ax2.plot(residuals, 'b.', label='Residuals', alpha=0.8)
+ax2.set_xlabel('Concatenated targets')
+ax2.set_ylabel('Residuals')
+ax2.axhline(0, color='black', linestyle='--', linewidth=1)  
+ax2.legend()
+
+plt.tight_layout()
+plt.show()
+
+residuals = valid_targets_wc - spectre_valid_wc
+print('MSE : ', np.sqrt((residuals**2).mean())*1e6, 'ppm')
